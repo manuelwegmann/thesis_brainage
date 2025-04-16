a2450f697734e41313a19fc0b906b1a2b35bbe5c
Manuel Wegmann <manuelw@alvis1.int.private>
2025-04-16 10:55:32 +0200

    Working on loader

--- scripts/loader.py
+++ scripts/loader.py
@@ -7,9 +7,16 @@ from torchvision.transforms.functional import InterpolationMode
 import torch
 import torchio as tio
 import numpy as np
+from .prep_data import full_data_load
 
 class loader3D(Dataset):
 
-    def __init__(self, args, trainvaltest):
+    def __init__(self, args, trainvaltest): 
+        self.demo = full_data_load() #fix full_data_load to be generalized
+        #no self-augmentation, correct?
+
+        #See  image-directory part in LILAC for to do, implement below
+
         
+
 
