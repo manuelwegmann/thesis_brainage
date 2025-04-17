14e1ed2669daf72596c094533720c232752f4ce2
Manuel Wegmann <manuelw@alvis1.int.private>
2025-04-17 15:48:37 +0200

    Work-in-progress data loader

--- scripts/loader.py
+++ scripts/loader.py
@@ -10,12 +10,29 @@ import numpy as np
 from .prep_data import full_data_load
 
 class loader3D(Dataset):
-
-    def __init__(self, args, trainvaltest): 
+    
+    #args: path to data, image size
+    def __init__(self, args): 
         self.demo = full_data_load() #fix full_data_load to be generalized
         #no self-augmentation, correct?
 
         #See  image-directory part in LILAC for to do, implement below
+        id_unique = np.unique(self.demo['participant_id'])
+        index_image_combination = np.empty((0, 2))
+        for id in id_unique:
+            indices = np.where(self.demo['participant_id'] == id)[0]
+            ### from here work in progress
+            tmp_combination = np.array(
+                np.meshgrid(np.array(range(len(indices))), np.array(range(len(indices))))).T.reshape(-1, 2)
+            index_image_combination = np.append(index_image_combination, indices[tmp_combination], 0)
+
+
+
+
+
+        #resize images
+        self.image_size = args.image_size 
+
 
         
 
